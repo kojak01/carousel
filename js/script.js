802b5492f96eda38712c19d7f50cca2135717e9f
Konrad Piatek <konrad.jerzy.piatek@gmail.com>
2022-10-25 20:29:42 +0200

    tablica

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const arr = [[3,21,37], [61,79,101,120], [133,149]];
+
+const isNumberInArrayofArrays = (number, array) => {
+  let found = false;
+  array.forEach(innerArray => {
+    if (innerArray.indexOf(number) !== -1) {
+      found = true;
+      return found;
+    }
+  }    
+  );
+  return found;
+};
+
+console.log(isNumberInArrayofArrays(0, arr)); //false
+console.log(isNumberInArrayofArrays(21, arr)); //true
+console.log(isNumberInArrayofArrays(221, arr)); //false
