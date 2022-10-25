4c5a93071a16375a7b72ccedf2e26f530e01ca57
Konrad Piatek <konrad.jerzy.piatek@gmail.com>
2022-10-26 01:01:43 +0200

    function

---
+++ js/functions.js
@@ -0,0 +1,21 @@
+function mnoZenie(arg1) {
+    return arg1 * 2
+}
+
+let result = mnoZenie(4);
+result = mnoZenie(5);
+result = mnoZenie(12);
+result = mnoZenie(14);
+console.log(result)
+
+function mnoZenieMnozenie (liczba1, liczba2) {
+   return liczba1 * liczba2
+}
+
+console.log(mnoZenieMnozenie(2,13));
+
+function potega(a) {
+    return a * a
+}
+
+console.log(potega(22));
